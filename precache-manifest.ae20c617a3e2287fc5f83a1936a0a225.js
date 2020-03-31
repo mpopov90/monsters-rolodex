a7acc9aabe8f8686ae3ca709c96e83f4afdc09ef
mpopov90 <mpopov90@gmail.com>
2020-03-31 19:16:37 +0200

    Updates

---
+++ precache-manifest.ae20c617a3e2287fc5f83a1936a0a225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d7a480342f1a7412d3e8cde4ad5929",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "103b077f5bf11df4670b",
+    "url": "/monsters-rolodex/static/css/main.4bfd58af.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103b077f5bf11df4670b",
+    "url": "/monsters-rolodex/static/js/main.9f0117cb.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
